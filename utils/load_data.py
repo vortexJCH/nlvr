006020dd227abf65748ba09bc96b86488442094a
vortexJCH <chuhao.j@gmail.com>
2019-05-04 22:26:03 +0800

    add some introduce for this repository

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
+# _*_coding:utf-8 _*_
+# @Author  : Chuhao Jin
+# @Email   : chuhao.j@gmail.com
+# @File    : load_data.py
+# @Time    : 2019/5/4 22:08
+#@Software: PyCharm
+
 import json
 
 def load_dataset(file_path):
-    return
+    return file_path
